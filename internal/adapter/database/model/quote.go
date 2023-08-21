1325dbc01d4e405346d20e0cbfba6aff8ded673c
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-08-22 06:23:54 +0800

    feat: change AuthorID to nullable

--- internal/adapter/database/model/quote.go
+++ internal/adapter/database/model/quote.go
@@ -10,7 +10,7 @@ type Quote struct {
 	ID           int        `gorm:"primaryKey" json:"id"`
 	QText        string     `gorm:"column:qtext;type:varchar(255);not null" json:"text"`
 	Tags         string     `gorm:"column:tags;type:varchar(255);not null" json:"tags"`
-	AuthorID     int        `json:"author_id"`
+	AuthorID     *int       `json:"author_id"`
 	Author       Author     `json:"author"`
 	Categories   []Category `gorm:"many2many:quote_categories"`
 	UserWhoLiked []User     `gorm:"many2many:quote_likes" json:"users_who_liked"`
