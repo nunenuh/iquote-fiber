b9176ef909df1cb27d540c7749c80b6a23808b47
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-08-23 17:16:03 +0800

    feat: add new method to quote repository interface

--- internal/domain/repository/quote_repository.go
+++ internal/domain/repository/quote_repository.go
@@ -4,11 +4,13 @@ import "github.com/nunenuh/iquote-fiber/internal/domain/entity"
 
 type IQuoteRepository interface {
 	GetAll(limit int, offset int) ([]*entity.Quote, error)
-	// GetByAuthor(name string) ([]*entity.Quote, error)
-	// GetByCategory(quote string) ([]*entity.Quote, error)
+	GetByAuthorName(name string, limit int, offset int) ([]*entity.Quote, error)
+	GetByAuthorID(ID int, limit int, offset int) ([]*entity.Quote, error)
+	GetByCategoryName(name string, limit int, offset int) ([]*entity.Quote, error)
+	GetByCategoryID(ID int, limit int, offset int) ([]*entity.Quote, error)
 	// GetByTags(tags string) ([]*entity.Quote, error)
 	// Search(keyword string) ([]*entity.Quote, error)
-	// Like(quoteID int, userID int) (*entity.Quote, error)
+	Like(quoteID int, userID int) (*entity.Quote, error)
 	// Unlike(quoteID int, userID int) (*entity.Author, error)
 	GetByID(ID int) (*entity.Quote, error)
 	Create(quote *entity.Quote) (*entity.Quote, error)
