2f316f7dd05763198a84e2acc0ccb2b2ba350898
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-08-23 17:18:00 +0800

    fix: change claims sub to claims user_id

--- internal/app/usecase/auth_interactor.go
+++ internal/app/usecase/auth_interactor.go
@@ -1,8 +1,9 @@
 package usecase
 
 import (
-	"errors"
+	"log"
 
+	exception "github.com/nunenuh/iquote-fiber/internal/app/exeption"
 	"github.com/nunenuh/iquote-fiber/internal/app/utils"
 	"github.com/nunenuh/iquote-fiber/internal/domain/entity"
 	"github.com/nunenuh/iquote-fiber/internal/domain/repository"
@@ -21,11 +22,18 @@ func NewAuthUsecase(repo repository.IUserRepository) *AuthUsecase {
 func (ucase *AuthUsecase) Login(username string, password string) (*entity.User, error) {
 	user, err := ucase.repo.GetByUsername(username)
 	if err != nil {
-		return nil, errors.New("Forbidden!")
+		return nil, exception.NewOtherError("Forbidden!")
 	}
 
+	log.Printf("username: %s, password: %s", username, user.Password)
+
+	// passwd, err := utils.HashPassword(password)
+	// if err != nil {
+	// 	return nil, exception.NewOtherError(err.Error())
+	// }
+
 	if !utils.CheckHashPassword(password, user.Password) {
-		return nil, errors.New("Invalid Credentials!")
+		return nil, exception.NewOtherError("Invalid Credentials!")
 	}
 
 	return user, nil
